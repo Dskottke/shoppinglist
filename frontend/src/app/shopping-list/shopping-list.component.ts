8ea041b0ad36cc7580b9730ad75a2b3d983f6c73
davidskottke <david.skottke@neuefische.de>
2023-09-11 14:18:43 +0200

    Add styling for a shopping-list item

--- frontend/src/app/shopping-list/shopping-list.component.ts
+++ frontend/src/app/shopping-list/shopping-list.component.ts
@@ -1,11 +1,12 @@
-import {Component, DoCheck, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Ingredient} from "./ingredient.model";
 import {IngredientApiService} from "./ingredient-api.service";
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent implements OnInit {
   shoppingList: Ingredient[] = []
@@ -24,9 +25,9 @@ export class ShoppingListComponent implements OnInit {
   }
 
   onDoneClick() {
-    const ingredientsDone : string[] =
-      this.shoppingList.filter((ingredient)=>ingredient.succeed === true)
-        .map((ingredient)=> ingredient.id)
+    const ingredientsDone: string[] =
+      this.shoppingList.filter((ingredient) => ingredient.succeed === true)
+        .map((ingredient) => ingredient.id)
     this.ingredientApiService.setDone(ingredientsDone)
   }
 }
